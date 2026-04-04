cbd0c347ae251711db02c6fe606448c5979a055e
DESKTOP-OG04IB7\HOME <atchoi.delacruz@gmail.com>
2026-04-04 21:00:22 +0800

    feat: add chibi warrior character assets and project configuration files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'duckrumble/arena',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '34d364428e92cf139e869285a016afa013f97d54',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'duckrumble/arena' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '34d364428e92cf139e869285a016afa013f97d54',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
